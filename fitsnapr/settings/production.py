b19df2f96bd7155ff74c20a1bf455ce2229330a7
basharatusman <basharatusman.a@gmail.com>
2020-07-04 13:15:00 -0400

    added AWS Cloudfront

--- fitsnapr/settings/production.py
+++ fitsnapr/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 import os
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['fitsnapr.herokuapp.com', '127.0.0.1']
 
@@ -9,17 +9,21 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_DEFAULT_ACL = None
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
+CLOUDFRONT_DOMAIN = os.environ.get('CLOUDFRONT_DOMAIN')
+CLOUDFRONT_ID = os.environ.get('CLOUDFRONT_ID')
+AWS_S3_CUSTOM_DOMAIN = CLOUDFRONT_DOMAIN
+
 STATIC_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+STATIC_URL = f'https://{CLOUDFRONT_DOMAIN}/{STATIC_LOCATION}/'
 STATICFILES_STORAGE = 'fitsnapr.storage_backends.StaticStorage'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (os.path.join('static'), )
 
+
 MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
+MEDIA_URL = f'https://{CLOUDFRONT_DOMAIN}/{MEDIA_LOCATION}/'
 DEFAULT_FILE_STORAGE = 'fitsnapr.storage_backends.PublicMediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- fitsnapr/storage_backends.py
+++ fitsnapr/storage_backends.py
@@ -11,3 +11,4 @@ class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
+
