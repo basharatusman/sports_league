7aaa17477b91afe70c6e1f9e307a28e3154fba10
basharatusman <basharatusman.a@gmail.com>
2020-07-04 00:28:46 -0400

    updated manage.py

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitsnapr.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
