6199cd59ba66b381b92c32d5b35ff001c7f10730
basharatusman <basharatusman.a@gmail.com>
2020-07-11 19:13:00 -0400

    added CORS Headers

--- fitsnapr/settings/base.py
+++ fitsnapr/settings/base.py
@@ -39,13 +39,15 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'commerce.apps.CommerceConfig',
     'storages',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- fitsnapr/settings/development.py
+++ fitsnapr/settings/development.py
@@ -10,3 +10,11 @@ STATICFILES_DIRS = (os.path.join('static'), )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]  # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
+CORS_ORIGIN_REGEX_WHITELIST = [
+    'http://localhost:3000',
+]
