e77ce57a1e1b8843aed722e1fc1fd3f808c0f8b6
basharatusman <basharatusman.a@gmail.com>
2020-08-20 16:32:48 -0400

    updated serializer

--- api/serializer_views.py
+++ api/serializer_views.py
@@ -17,9 +17,9 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class UserProfileView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
-    # queryset = UserProfile.objects.filter(user_id=1)
+    # queryset = UserProfile.objects.filter(user_id=14)
     serializer_class = ProfileSerializer
 
     def get_queryset(self, *args, **kwargs):
         pk = self.request.query_params.get('id')
-        return UserProfile.objects.filter(user_id=pk)
+        return UserProfile.objects.filter(user_id=14)
